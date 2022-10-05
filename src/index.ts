9b40e20b16fdabd3e5d935dc78546d18bf25eaa6
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2022-10-05 07:11:21 +0200

    Initial config

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+import "reflect-metadata";
+import express from "express";
+dotenv.config();
+
+function bootstrap() {
+  // Build the schema
+  // Initialize express
+  // Create apollo server
+  // await server.start()
+  // apply middleware to server
+  // app.listen on express server
+  // connect to DB
+}
+
+bootstrap();
+
+console.log("Hello");
